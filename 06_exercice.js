477f651e1012801d8bb910c28dc139af088e9be6
Patrick Lauzon <patrick.lauzon94@hotmail.com>
2017-02-28 14:54:39 -0500

    creation function gnTableauProvince

--- 06_exercice.js
+++ 06_exercice.js
@@ -8,7 +8,29 @@ fs.readFile('06_fichier.json', 'utf8', function (err, data){
 	console.log(objet.toString());
 });
 
+function genTableauProvince(){
 
+	var tab = "";
+
+	tab += "<h1>Tableau de provinces</h1>";
+	tab += "<table>";
+	for (province in objet){
+
+		tab += "<tr><td>" + province + "</td><td>" + objet[province].toString() + "</td></tr>";
+	}
+	tab += "</table>";
+
+	return tab;
+}
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html; charset=utf8"});
+  
+
+  response.write(ecritureTableauProvince());	
+
+  response.end(); 
+}).listen(8888);
 
 
 
