5a3f9821f97dca28e0d499d56ee5ba04e4c27756
Patrick Lauzon <patrick.lauzon94@hotmail.com>
2017-02-28 14:52:19 -0500

    .readFile

--- 06_exercice.js
+++ 06_exercice.js
@@ -2,6 +2,14 @@ var fs = require("fs"); // importation du module de gestion de fichiers
 var http = require("http"); // importation du module de serveur http
 
 var objet;
+fs.readFile('06_fichier.json', 'utf8', function (err, data){
+	if(err) return console.error(err);
+	objet = JSON.parse(data);
+	console.log(objet.toString());
+});
+
+
+
 
 
 console.log("Program Ended");
